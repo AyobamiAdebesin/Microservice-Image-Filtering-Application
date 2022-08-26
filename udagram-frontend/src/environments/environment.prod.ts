2c76c8dfb9d8a4f70516ed16586919d8ba0bf8e7
AyobamiAdebesin <froshofficial@gmail.com>
2022-08-26 06:11:51 +0100

    [Refactor] Modified the endpoints with the exposed external IP

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'af02486428c7e4c9fbc21329e5fbe1e5-326094921.us-east-2.elb.amazonaws.com/api/v0'
 };
 
 /*
